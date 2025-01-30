5133886f564b69920aee9d7ed856dac84dd4bfe7
Aminodiin1995 <amin.hassan@d-money.dj>
2025-01-30 14:29:53 +0300

    tenth

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // Cloudflare specific config
+    experimental: {
+      isrMemoryCacheSize: 0, // Recommended for Cloudflare Pages
+    },
+    async redirects() {
+      return [
+        {
+          source: '/:path*', // This will catch all paths, not just root
+          has: [
+            {
+              type: 'host',
+              value: 'www.sahan-hub.com',
+            },
+          ],
+          destination: 'https://sahan-hub.com/:path*',
+          permanent: true,
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
